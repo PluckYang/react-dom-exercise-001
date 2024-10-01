9f3a8ce211b7f44147d0694ed2044a2a84761728
Pluck Yang <pluck.yang@gmail..com>
2024-10-01 17:57:10 +0900

    Enhancing the "Result Modal"

--- src/components/ResultModal.js
+++ src/components/ResultModal.js
@@ -1,10 +1,13 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime }, //原本function的参数
+  { targetTime, remainingTime, onReset }, //原本function的参数
   ref //添加的ref参数
 ) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
   useImperativeHandle(ref, () => {
     return {
@@ -16,14 +19,15 @@ const ResultModal = forwardRef(function ResultModal(
 
   return (
     <dialog className="result-modal" ref={dialog}>
-      <h2>You {result}</h2>
+      {userLost && <h2>You lost</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stop the timer with <strong>X second left.</strong>
+        You stop the timer with{" "}
+        <strong>{formattedRemainingTime} second left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
